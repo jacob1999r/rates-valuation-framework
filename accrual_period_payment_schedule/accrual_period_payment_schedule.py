75a20d6087193770e9a944ef7a74a7d4e6d63829
jacob1999r <jacobrussell@hotmail.co.uk>
2026-01-02 13:03:26 +0000

    Implemented function to generate accrual periods, including with stub periods. Does not yet consider unorthodox period end dates such as bank holidays.

---
+++ accrual_period_payment_schedule/accrual_period_payment_schedule.py
@@ -0,0 +1,25 @@
+from datetime import date
+import math
+from dateutil.relativedelta import relativedelta
+
+def generate_schedule(start_date, end_date, frequency):
+    #takes frequency as the number of months between payments to determine accrual periods and payment dates, outputting a list of 3-tuples (accrual start, accrual end and payment date)
+    schedule = []
+    #run a while loop until period end date surpasses or equals the end date, then break and consider stub period
+    #timedelta = end_date - start_date
+    #n = math.ceil(timedelta.months/frequency)
+    period_start = start_date
+    period_end = period_start + relativedelta(months=frequency)
+    while period_end < end_date:
+
+        #to do, payment date computation taking into consideration bank holidays etc.
+        payment_date = period_end
+
+        schedule.append((period_start, period_end, payment_date))
+        period_start = period_end
+        period_end = period_start + relativedelta(months=frequency)
+
+    #stub period, again needs modification for payment date
+    if period_start != end_date:
+        schedule.append((period_start, end_date, end_date))
+    return schedule
---
+++ changelog.md
@@ -0,0 +1,6 @@
+02/01/2026 v0.2 - Implemented functionality for generating a list of payment schedules when given a start date, end date and payment frequency. 
+
+
+
+01/01/2026 v0.1 - Implemented functionality for computing year fractions from different day count conventions, specifically ACT/360, ACT/365 and 30E/360.
+
